087ea2714c367582cb85e29d90fe68af8d8d3289
Kevin O'Neil <kevinmoneil@live.com>
2018-06-22 14:29:20 +0900

    cleans up server app file

--- server/app.js
+++ server/app.js
@@ -1,32 +1,8 @@
 const express = require("express");
-const passport = require("passport");
-const OpenIDStrategy = require("passport-openid").Strategy;
-const clientId = process.env.CLIENT_ID;
-const clientSecret = process.env.SECRET;
 const app = express();
 
-passport.use(
-  new OpenIDStrategy({
-    returnURL: 'http://localhost:3000/auth/openid/return',
-    realm: 'http://localhost:3000/'
-  },
-  function(identifier, done) {
-    return done(err, user);
-    // User.findByOpenID({ openId: identifier }, function (err, user) {
-    //   return done(err, user);
-    // });
-  }
-));
-
-app.post('/auth/openid',
-  passport.authenticate('openid'));
-
-app.get('/auth/openid/return', 
-  passport.authenticate('openid', { failureRedirect: '/login' }),
-  function(req, res) {
-    // Successful authentication, redirect home.
-    res.redirect('/');
-  }
-);
+app.get('/login', () => {
+  console.log("hello");
+});
 
 app.listen(3000);
