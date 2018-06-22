78a2283689f22542b5339ba52ab72274a834332a
Kevin O'Neil <kevinmoneil@live.com>
2018-06-22 15:03:36 +0900

    adds typescript and loaders

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,10 +3,23 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
-  entry: 'src/index.js',
+  entry: path.resolve(__dirname, './src/index.ts'),
+  devtool: 'inline-source-map',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js']
   },
   plugins: [
     new HtmlWebpackPlugin({
