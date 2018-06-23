0f542f01b5769eb9e53b4ef90236b2d1f5bd8fce
Kevin O'Neil <kevinmoneil@live.com>
2018-06-23 13:43:22 +0900

    creates Title component file

--- client/src/containers/App.tsx
+++ client/src/containers/App.tsx
@@ -1,9 +1,5 @@
 import * as React from 'react';
-import styled from 'styled-components';
-
-const Title = styled.h1`
-  border-bottom: 1px solid green;
-`;
+import Title from "../components/Title";
 
 export default class App extends React.Component {
   render() {
