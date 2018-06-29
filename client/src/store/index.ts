b57e76234070fe2545c2d8a2c930d70f2000719f
Kevin O'Neil <kevinmoneil@live.com>
2018-06-29 19:50:18 +0900

    adds check for env variable

--- client/src/store/index.ts
+++ client/src/store/index.ts
@@ -4,7 +4,9 @@ import { composeWithDevTools } from 'redux-devtools-extension';
 import { createLogger } from 'redux-logger';
 
 const logger = createLogger();
-const middlewares = [logger];
+const middlewares = process.env.NODE_ENV === 'development' 
+  ? [logger]
+  : [];
 
 const enhancers = composeWithDevTools(
   applyMiddleware(...middlewares),
