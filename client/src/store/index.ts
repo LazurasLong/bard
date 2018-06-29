15e3ad94a04196f212c78ccc0a335c7fcdf47d7f
Kevin O'Neil <kevinmoneil@live.com>
2018-06-29 19:47:51 +0900

    adds redux devtools

--- client/src/store/index.ts
+++ client/src/store/index.ts
@@ -1,16 +1,16 @@
 import { username } from './reducers';
-import persistState from 'redux-localstorage'
 import { compose, applyMiddleware, createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import { createLogger } from 'redux-logger';
 
 const logger = createLogger();
+const middlewares = [logger];
 
-const enhancer = compose(
-  logger,
-  persistState()
+const enhancers = composeWithDevTools(
+  applyMiddleware(...middlewares),
 );
 
 export default createStore(
   username,
-  applyMiddleware(logger)
+  enhancers
 );
