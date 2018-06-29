d153790a43047cde14c3b8ce021eb99a3f0ecce5
Kevin O'Neil <kevinmoneil@live.com>
2018-06-30 00:57:12 +0900

    adds apollo packages

--- server/app.js
+++ server/app.js
@@ -1,8 +1,47 @@
-const express = require("express");
+const express = require('express');
+const apollo = require('apollo-server-express');
+const bodyParser = require('body-parser');
+
+const { graphqlExpress, graphiqlExpress } = require('apollo-server-express');
+const { makeExecutableSchema } = require('graphql-tools');
+
 const app = express();
 
-app.get('/login', () => {
-  console.log("hello");
-});
+const adventures = [
+  {
+    title: 'Ravencall Tower',
+    number: 1,
+    campaign: 'Ley of the Land'
+  },
+  {
+    title: 'The Holder Below',
+    number: 2,
+    campaign: 'Ley of the Land'
+  }
+];
+
+const typeDefs = `
+  type Adventure { 
+    title: String, 
+    number: Int, 
+    campaign: String 
+  }
+
+  type Query { 
+    adventures: [Adventure] 
+  }
+`
+
+const resolvers = {
+  Query: { adventures: () => adventures },
+}
+
+const schema = makeExecutableSchema({
+  typeDefs,
+  resolvers
+})
+
+app.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
+app.get('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
 
 app.listen(3000);
