9031461ad906347b8d09664ec6f9a0582997253f
Kevin O'Neil <kevinmoneil@live.com>
2018-06-29 23:45:42 +0900

    adds max-width property

--- client/src/components/CenteredWrapper.tsx
+++ client/src/components/CenteredWrapper.tsx
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 export default styled.section`
   display: flex;
   width: 75%;
+  max-width: 500px;
   margin: 0 auto;
-  margin-top: 10vh;
+  margin-top: 20vh;
 `;
