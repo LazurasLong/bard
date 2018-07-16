0e7db4d4ad14a9f28cb4e55a17a6480d37f52292
Kevin O'Neil <kevinmoneil@live.com>
2018-07-16 12:21:30 +0900

    moves pool to a separate file, adds postgres query to get all users

--- server/data/schema.js
+++ server/data/schema.js
@@ -1,3 +1,4 @@
+const db = require('../db');
 const find = require('lodash/find');
 const { makeExecutableSchema } = require('graphql-tools');
 
@@ -7,10 +8,6 @@ const typeDefs = `
     name: String!,
     isAdmin: Boolean,
     adventures: [Adventure]
-  }
-
-  type Users {
-    users: [User]
   }
 
   type Adventure { 
@@ -34,13 +31,38 @@ const typeDefs = `
   }
 `;
 
+async function getUsers() {
+  try {
+    const res = await db.query(`SELECT * from users`);
+
+    return res.rows;
+  } catch (err) {
+    throw new Error("Could not retrieve users!");
+  }
+}
+
+async function insertUser(id, name) {
+  try {
+    const res = await db.query(`INSERT INTO users VALUES('${id}', '${name}', ${false});`);
+
+    return res;
+  } catch (err) {
+    throw new Error('User already exists');
+  }
+}
+
 const resolvers = {
   Query: { 
+    users: async function () {
+      const res = await getUsers();
+      return res;
+    },
     user: (_, { name }) => find(users, { id }),
   },
   
   Mutation: {
     createUser: (_, { id, name }) => {
+      insertUser(id, name);
       return { id, name };
     }
   }
